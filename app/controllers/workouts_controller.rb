9a7081a2c914908a19a083742b0f55634842b928
Andrew Woo <ahjwoo@gmail.com>
2016-01-22 13:11:32 -0800

    Add edit and destroy ability

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -19,11 +19,20 @@ class WorkoutsController < ApplicationController
 			render 'new'
 	end
 end
-
-	def update
+	def edit
 	end
 
+	def update
+		if @workout.update(workout_params)
+			redirect_to @workout
+		else
+			render 'edit'
+	end
+end
+
 	def destroy
+		@workout.destroy
+		redirect_to root_path
 	end
 
 private
